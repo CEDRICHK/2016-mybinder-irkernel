5067be6bad790cc469e5a528813f3df65cf75f98
CEDRICHK <31773692+CEDRICHK@users.noreply.github.com>
2018-07-12 17:11:54 +0200

    Update install-irkernel.R

--- install-irkernel.R
+++ install-irkernel.R
@@ -1,4 +1,9 @@
-install.packages(c('crayon', 'pbdZMQ', 'devtools'))
-devtools::install_github(paste0('IRkernel/', c('repr', 'IRdisplay', 'IRkernel')))
+# Based on https://irkernel.github.io/installation/
 
-IRkernel::installspec()
+# Step 1: Installing packages and dependencies via supplied binary packages
+update.packages(repos='http://cran.us.r-project.org', ask=FALSE)
+install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest', 'ISwR'), repos='http://cran.us.r-project.org', dependencies = TRUE)
+devtools::install_github('IRkernel/IRkernel')
+
+# Step 2: Making the kernel available to Jupyter
+IRkernel::installspec(user = FALSE)
